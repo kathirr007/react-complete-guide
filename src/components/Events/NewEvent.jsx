280049ad0b6c9366781557e852df4da10a3c0a60
Kathiravan K <kathirr007@gmail.com>
2025-01-06 16:09:24 +0530

    feat(event-details): Event details updated to get event details

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -2,11 +2,27 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useMutation } from '@tanstack/react-query';
+import { createNewEvent } from '@/util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../util/http.js';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { isPending, mutateAsync, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events']
+      });
+      navigate('/events');
+    }
+  });
+
+  async function handleSubmit(formData) {
+    await mutateAsync({ event: formData });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
@@ -15,11 +31,16 @@ export default function NewEvent() {
           <Link to="../" className="button-text">
             Cancel
           </Link>
-          <button type="submit" className="button">
-            Create
+          <button type="submit" className="button text-2xl">
+            {isPending ? 'Submitting...' : 'Create'}
           </button>
         </>
       </EventForm>
+      {
+        isError && (
+          <ErrorBlock title="Failed to create event" message={error.info?.message || 'Failed to create event, please check your inputs and try again later.'} />
+        )
+      }
     </Modal>
   );
 }
--- src/components/ImagePicker.jsx
+++ src/components/ImagePicker.jsx
@@ -1,12 +1,17 @@
 export default function ImagePicker({ images, selectedImage, onSelect }) {
+  function handleKeyDown(e, image) {
+    if (e.key === 'Enter') { onSelect(image.path); }
+  }
   return (
     <div id="image-picker">
       <p>Select an image</p>
       <ul>
-        {images.map((image) => (
+        {images.map(image => (
           <li
             key={image.path}
             onClick={() => onSelect(image.path)}
+            tabIndex={0}
+            onKeyDown={e => handleKeyDown(e, image)}
             className={selectedImage === image.path ? 'selected' : undefined}
           >
             <img
